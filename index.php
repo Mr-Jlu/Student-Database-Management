c1d0cf41d22e91b7d8dc6e10694dd3c00dbc285b
Ajaykumar Majukar <31211096+Mr-Jlu@users.noreply.github.com>
2017-10-23 22:55:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,14 @@
     <title>SDM APPLICATION - INDEX PAGE</title>
     
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://localhost/SDM/css/bootstrap.min.css" crossorigin="anonymous">
     
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
     crossorigin="anonymous"></script>
+    <script src="http://localhost/SDM/js/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+     <script src="http://localhost/SDM/js/bootstrap.min.js" crossorigin="anonymous"></script>
     
     
 </head>
