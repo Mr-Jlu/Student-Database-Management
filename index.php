2d40f42cc4c805b4107716e18cd919f9c2c37016
Ajaykumar Majukar <31211096+Mr-Jlu@users.noreply.github.com>
2017-10-24 09:37:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,18 +5,23 @@
     <title>SDM APPLICATION - INDEX PAGE</title>
     
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link rel="stylesheet" href="http://localhost/SDM/css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
     
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
     crossorigin="anonymous"></script>
-    <script src="http://localhost/SDM/js/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
+    <script src="./js/jquery-3.2.1.min.js"></script>
 
+    
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-     <script src="http://localhost/SDM/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <script src="./js/bootstrap.min.js"></script>
+     
     
+   <link rel="stylesheet" href="./css/custom.css">
     
+      
 </head>
 <body>
-    
+   
+         
 </body>
 </html>
